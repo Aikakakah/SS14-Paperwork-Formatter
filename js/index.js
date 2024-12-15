4e230e5e7507ce98daa6c892bb5afaaa087e2ee3
Aikakakah <145503852+Aikakakah@users.noreply.github.com>
2024-12-14 21:30:54 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -61,7 +61,7 @@ function parseBBCode(text) {
 
     // Newlines
     parsedText = parsedText.replace(/\n/g, '<br>');
-    parsedText = parsedText.replace(/\[\/head]<br>/g, '[/head]');
+    //parsedText = parsedText.replace(/\[\/head]<br>/g, '[/head]');
 
     // Bold
     parsedText = parsedText.replace(/\[bold](.*?)\[\/bold]/gis, '<strong>$1</strong>');
